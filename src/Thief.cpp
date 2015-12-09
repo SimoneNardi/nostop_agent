37ce43539e2dc0f7f7edb4b92e95ee77b2840164
Simone Nardi <monena@tiscali.it>
2015-12-09 19:30:28 +0100

    nodo e namespace

--- src/Thief.cpp
+++ src/Thief.cpp
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 	std::string l_name = "thief";
 	
 	// get the ID from Simulator:
-	ros::NodeHandle l_nodeID;
+	ros::NodeHandle l_nodeID("~");
 	ros::ServiceClient l_clientID = l_nodeID.serviceClient<nostop_agent::PlayerIDData>("/simulator/thief/id");
 	nostop_agent::PlayerIDData l_srvID;
 	l_srvID.request.name = l_name;
